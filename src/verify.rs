8e66db808c0b5f3a8318739904aeb4395c68d4ad
version513 <seuair@gmail.com>
2024-10-05 22:06:21 +0800

    remove deprecated bls-unchained-on-g1 scheme

--- src/verify.rs
+++ src/verify.rs
@@ -1,6 +1,6 @@
 //! # verify
 //!
-//! this module contains some of the cruptographic internals that some users might wish to use
+//! this module contains some of the cryptographic internals that some users might wish to use
 //! manually without the client
 //!
 
@@ -32,7 +32,6 @@ const DST_G2: &str = "BLS_SIG_BLS12381G2_XMD:SHA-256_SSWU_RO_NUL_";
 pub enum SchemeID {
     PedersenBlsChained,
     PedersenBlsUnchained,
-    UnchainedOnG1,
     UnchainedOnG1RFC9380,
 }
 
@@ -45,14 +44,12 @@ impl<'de> Deserialize<'de> for SchemeID {
         match s {
             "pedersen-bls-chained" => Ok(SchemeID::PedersenBlsChained),
             "pedersen-bls-unchained" => Ok(SchemeID::PedersenBlsUnchained),
-            "bls-unchained-on-g1" => Ok(SchemeID::UnchainedOnG1),
             "bls-unchained-g1-rfc9380" => Ok(SchemeID::UnchainedOnG1RFC9380),
             _ => Err(serde::de::Error::unknown_variant(
                 s,
                 &[
                     "pedersen-bls-chained",
                     "pedersen-bls-unchained",
-                    "bls-unchained-on-g1",
                     "bls-unchained-g1-rfc9380",
                 ],
             )),
@@ -93,12 +90,6 @@ pub fn verify_beacon(
             DST_G2,
         ),
         SchemeID::PedersenBlsUnchained => verify_on_g2(
-            public_key,
-            &unchained_beacon_message(beacon)?,
-            &beacon.signature,
-            DST_G2,
-        ),
-        SchemeID::UnchainedOnG1 => verify_on_g1(
             public_key,
             &unchained_beacon_message(beacon)?,
             &beacon.signature,
@@ -487,22 +478,6 @@ mod test {
             verify_beacon(&SchemeID::PedersenBlsUnchained, &public_key, &beacon),
             VerificationError::InvalidPublicKey,
         );
-    }
-
-    #[test]
-    fn g1g2_swap_non_rfc_beacon_verifies() {
-        let public_key = dehexify("a0b862a7527fee3a731bcb59280ab6abd62d5c0b6ea03dc4ddf6612fdfc9d01f01c31542541771903475eb1ec6615f8d0df0b8b6dce385811d6dcf8cbefb8759e5e616a3dfd054c928940766d9a5b9db91e3b697e5d70a975181e007f87fca5e");
-        let beacon = Beacon {
-            round_number: 3,
-            randomness: dehexify("a4eb0ed6c4132da066843c3bfdce732ce5013eda86e74c136ab8ccc387b798dd"),
-            signature: dehexify("8176555f90d71aa49ceb37739683749491c2bab15a46094b255289ed25cf8f01cdfb1fe8bd9cd5a19eb09448a3e53186"),
-            previous_signature: Vec::new(),
-        };
-
-        assert!(matches!(
-            verify_beacon(&SchemeID::UnchainedOnG1, &public_key, &beacon),
-            Ok(_)
-        ));
     }
 
     #[test]
